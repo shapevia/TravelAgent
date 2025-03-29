9a6a32ea7b128fd33300a0de1bb62e39fdd1b669
shapevia <info@shapevia.com>
2025-03-29 03:25:22 +0100

    Aggiornato travel_data.pkl con activities

--- create_data.py
+++ create_data.py
@@ -1,38 +1,32 @@
 import pandas as pd
 import pickle
 
-# Dati più realistici e dettagliati
 data = {
-    'id': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+    'id': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     'destination': [
-        'Positano, Costiera Amalfitana', 'Chamonix, Alpi Francesi', 'New York City, USA', 
-        'Kyoto, Giappone', 'Maasai Mara, Kenya', 'Banff National Park, Canada', 
+        'Positano, Italia', 'Chamonix, Francia', 'New York, USA', 
+        'Kyoto, Giappone', 'Maasai Mara, Kenya', 'Banff, Canada', 
         'Barcellona, Spagna', 'Santorini, Grecia', 'Bali, Indonesia', 
-        'Reykjavik e Aurora Boreale, Islanda', 'Venezia, Italia', 'Cape Town, Sudafrica', 
-        'Marrakech, Marocco', 'Queenstown, Nuova Zelanda', 'Lisbona, Portogallo'
+        'Reykjavik, Islanda'
     ],
-    'price': [
-        950, 1300, 1800, 1600, 2000, 1100, 850, 1200, 900, 
-        1400, 700, 1500, 800, 1700, 750
-    ],
-    'duration_days': [
-        5, 7, 6, 8, 10, 7, 4, 5, 7, 
-        6, 3, 8, 5, 9, 4
-    ],
+    'price': [950, 1300, 1800, 1600, 2000, 1100, 850, 1200, 900, 1400],
+    'duration_days': [5, 7, 6, 8, 10, 7, 4, 5, 7, 6],
     'tags': [
-        ['spiaggia', 'relax', 'cibo'], ['montagna', 'avventura', 'natura'], ['città', 'cultura'], 
-        ['cultura', 'cibo', 'relax'], ['avventura', 'natura', 'safari'], ['natura', 'montagna', 'relax'], 
-        ['città', 'cultura', 'spiaggia'], ['spiaggia', 'relax', 'cultura'], ['spiaggia', 'natura', 'relax'], 
-        ['natura', 'avventura', 'relax'], ['città', 'cultura'], ['città', 'natura', 'avventura'], 
-        ['cultura', 'cibo'], ['avventura', 'montagna', 'natura'], ['città', 'cibo', 'cultura']
+        ['spiaggia', 'relax', 'cibo'], ['montagna', 'avventura', 'sci'], ['città', 'cultura', 'shopping'], 
+        ['cultura', 'cibo', 'tempio'], ['avventura', 'natura', 'safari'], ['natura', 'montagna', 'escursioni'], 
+        ['città', 'spiaggia', 'arte'], ['spiaggia', 'relax', 'tramonti'], ['spiaggia', 'natura', 'yoga'], 
+        ['natura', 'avventura', 'aurora']
+    ],
+    'activities': [
+        ['Gita in barca', 'Cena sul mare'], ['Sci alpino', 'Escursione Monte Bianco'], ['Tour Broadway', 'Shopping 5th Ave'], 
+        ['Visita templi', 'Cerimonia del tè'], ['Safari jeep', 'Campo tendato'], ['Trekking', 'Canoa sul lago'], 
+        ['Sagrada Familia', 'Spiaggia Barceloneta'], ['Tour Oia', 'Bagno termale'], ['Templi Uluwatu', 'Yoga'], 
+        ['Tour Golden Circle', 'Caccia aurora']
     ]
 }
 
-# Crea un DataFrame
 travel_data = pd.DataFrame(data)
-
-# Salva in travel_data.pkl
 with open('travel_data.pkl', 'wb') as f:
     pickle.dump(travel_data, f)
 
-print("Dataset aggiornato con successo!")
+print("Dataset aggiornato con attività!")
